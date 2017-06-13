f23639face05b2138f14553d7b2834ee622fc6fc
Szymon <wictusn@gmail.com>
2017-06-13 11:59:41 +0200

    Add sources class

--- sources.cpp
+++ sources.cpp
@@ -1,7 +1,53 @@
 #include "sources.h"
 
+sources::sources()
+{
+
+}
+
 sources::sources(const std::string description, const double activity, const double energy)
 {
   fDescription = description;
+  fActivity =activity;
+  fEnergy = energy;
 }
+
+sources::sources(const sources& source)
+{
+  fActivity = source.getActivity();
+  fEnergy = source.getEnergy();
+  fDescription = source.getDescription();
+}
+
+
+double sources::getActivity() const
+{
+  return fActivity;
+}
+
+std::string sources::getDescription() const
+{
+  return fDescription;
+}
+
+double sources::getEnergy() const
+{
+  return fEnergy;
+}
+
+double sources::setActivity(const double activity) 
+{
+  fActivity = activity;
+}
+
+std::string sources::setDescription(const std::string description) 
+{
+  fDescription = description;
+}
+
+double sources::setEnergy(const double energy) 
+{
+  fEnergy = energy;
+}
+
 
--- sources.h
+++ sources.h
@@ -7,6 +7,14 @@ class sources
 {
 public:
   sources(const std::string description, const double activity, const double energy); 
+  sources( const sources& source);
+  sources();
+  double getEnergy() const;
+  double getActivity() const;
+  std::string getDescription() const;
+  double setEnergy(const double energy);
+  double setActivity(const double activity);
+  std::string setDescription(const std::string description);
 private:
   std::string fDescription ="";
   double fEnergy = 0;
