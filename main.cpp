c090c8bd705f9c1e77232dc58c39fb348db946b6
Szymon <wictusn@gmail.com>
2017-06-13 14:32:32 +0200

    Make program take input data as specified by sushil

--- main.cpp
+++ main.cpp
@@ -4,16 +4,26 @@
 #include "doseCalculator.h"
 
 int main(int argc, char **argv) 
-{  
+{ 
+  if(argc != 3)
+  {
+    std::cout << "Please add input parameters: escape ratio and distance from source e.g:\n"
+    << "./doseCalculator 2.5 40\n";
+    std::exit(2); 
+  }
+  
   sources neutronBeam("neutron source of 14 MeV monochromatic neutrons, isotropic", 1E9, 14);
   
-  shieldings paraffinBox("parrafin box with set 40 cm thicnkess and escape ratio of 5.5%", sarcophagus, 27.54, 0.02); 
+  shieldings paraffinBox("parrafin box", 
+			 sarcophagus, 0, atof(argv[1]) ); 
   paraffinBox.setSource(neutronBeam);
   
   doseCalculator calculator;
   calculator.setShielding(paraffinBox);
-  calculator.setDistance(100);
-  std::cout << calculator.getDose() << std::endl;
+  calculator.setDistance( atof(argv[2]));
+  calculator.setConversionCoefficient(450);
+  std::cout <<"For escape ratio: " << atof(argv[1]) << "% and distance from the source " 
+  << atof(argv[2]) << "cm dose in [mSv/week] amounts to " << calculator.getDose() << std::endl;
   
   return 0;
 }
