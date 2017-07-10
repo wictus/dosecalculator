1fa13bef9b4f651a7e57afa7d5a9d65499f72d8b
Szymon <wictusn@gmail.com>
2017-07-10 18:08:36 +0200

    Write example of using dose map

--- main.cpp
+++ main.cpp
@@ -2,28 +2,58 @@
 #include "sources.h"
 #include "shieldings.h"
 #include "doseCalculator.h"
+#include "doseMap.h"
+
+void exampleOfSingleUse(const double escapeRatio, const double distanceFromSource);
 
 int main(int argc, char **argv) 
 { 
-  if(argc != 3)
+  exampleOfSingleUse(2.5, 1);
+  exampleOfSingleUse(2.5, 6);
+  
+  //doseMap, set dimensions of room
+  doseMap map(575, 737);
+  //set coordinates of place
+  map.setSourcePosition(75, 200);
+  //set calculator stuff
+  sources neutronBeam("neutron source of 14 MeV monochromatic neutrons, isotropic",
+		      7.7E5, 14);
+  shieldings paraffinBox("parrafin box", 
+			 sarcophagus, 50, 2.5 ); 
+  paraffinBox.setSource(neutronBeam);
+  doseCalculator calculator;	
+  calculator.setShielding(paraffinBox);
+  calculator.setDistance( 0 );
+  calculator.setConversionCoefficient(384);
+  
+  map.setCalculator(calculator);
+  //create map
+  map.createADoseMap();	
+    
+  return 0;
+}
+
+void exampleOfSingleUse(const double escapeRatio, const double distanceFromSource)
+{
+  if( 0 == escapeRatio && 0 == distanceFromSource )
   {
-    std::cout << "Please add input parameters: escape ratio and distance from source e.g:\n"
-    << "./doseCalculator 2.5 40\n";
+    std::cout << "Please add input parameters: escape ratio and distance from source\n";
     std::exit(2); 
   }
   
   sources neutronBeam("neutron source of 14 MeV monochromatic neutrons, isotropic", 1E9, 14);
   
   shieldings paraffinBox("parrafin box", 
-			 sarcophagus, 0, atof(argv[1]) ); 
+			 sarcophagus, 0, escapeRatio ); 
   paraffinBox.setSource(neutronBeam);
   
   doseCalculator calculator;
   calculator.setShielding(paraffinBox);
-  calculator.setDistance( atof(argv[2]));
+  calculator.setDistance( distanceFromSource);
   calculator.setConversionCoefficient(450);
-  std::cout <<"For escape ratio: " << atof(argv[1]) << "% and distance from the source " 
-  << atof(argv[2]) << "cm dose in [mSv/week] amounts to " << calculator.getDose() << std::endl;
-  
-  return 0;
+  std::cout <<"For escape ratio: " << escapeRatio << "% and distance from the source " 
+  << distanceFromSource << "cm dose in [mSv/week] amounts to " << calculator.getDose() << std::endl;
+
 }
+
+
